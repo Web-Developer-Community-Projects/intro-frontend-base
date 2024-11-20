f2c6b1a1bc979fd63d5189ee501abf05a3c5a9bf
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-11-20 17:39:07 -0300

    add lautaro

--- src/presentation/pages/LautaroPage.ts
+++ src/presentation/pages/LautaroPage.ts
@@ -1,10 +1,28 @@
+import { Avatar } from "../components/shared";
+
 export function LautaroPage(){
   const $containerLautaro = document.createElement("section");
-
+  $containerLautaro.classList.add("estilos")
+  const $estilos = document.createElement("style")
   $containerLautaro.innerHTML = /*html*/`
-    <h1>Lautaro</h1>
+    <h1 >Lautaro</h1>
   `;
 
+  $estilos.innerHTML = /*html*/ `
+  .estilos{
+    h1{
+      cursor:pointer;
+      &:hover{
+        color: red;
+      }
+    }
+  }
+  `
+  $containerLautaro.appendChild(Avatar({
+    imgUrl: "/public/avatar.png" ,
+    fallback: "Lautaro"
+  }))
 
+  $containerLautaro.append($estilos)
   return $containerLautaro;
 }
