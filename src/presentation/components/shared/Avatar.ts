3c8fc864b5e4efd38c53f7e7ada8f8b5abc2546d
jonatan <jonalaual_2000@hotmail.com>
2024-11-19 23:04:39 -0500

    feat: se ha agregado iniciales de nombre cuando la imagen esta rota

--- src/presentation/components/shared/Avatar.ts
+++ src/presentation/components/shared/Avatar.ts
@@ -11,6 +11,20 @@ export function Avatar({imgUrl,fallback = "Avatar Perfil"}: AvatarProps) {
 
 
   // TODO: Crear funcionalidad de aparecer iniciales de fallback en caso de no tener imagen
+  $avatar.onerror = () => {
+    const initials = fallback
+      .split(' ')
+      .map(word => word[0])
+      .join('')
+      .toUpperCase();
 
+    const $textFallback = document.createElement('div');
+    $textFallback.textContent = initials;
+    $textFallback.setAttribute('role', 'img'); 
+    $textFallback.setAttribute('aria-label', fallback);
+
+    $avatar.replaceWith($textFallback);
+  };
+  
   return $avatar;
 }
--- src/presentation/pages/JonatanPage.ts
+++ src/presentation/pages/JonatanPage.ts
@@ -1,3 +1,5 @@
+import { Avatar } from "../components/shared";
+
 export function JonatanPage(){
   const $containerJonatan = document.createElement("section");
 
@@ -5,6 +7,10 @@ export function JonatanPage(){
     <h1>Jonatan</h1>
   `;
 
+  $containerJonatan.appendChild(Avatar({
+    imgUrl: "/avatar.png",
+    fallback: "Jonatan Laureano"
+  }));
 
   return $containerJonatan;
 }
